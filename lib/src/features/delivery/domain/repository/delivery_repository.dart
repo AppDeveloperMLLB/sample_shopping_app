4526bda9815f33bb5593ed1da59810b2ea7bd777
MLLB <app.developer.mllb@gmail.com>
2023-02-13 18:17:30 +0900

    不要なファイルを削除

--- lib/src/features/delivery/domain/repository/delivery_repository.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:sample_shopping_app/src/features/delivery/domain/order.dart';
-
-// abstract class DelivelyRepository {
-//   Future<List<Order>> getOrderList();
-//   Future<void> markOrderAsDeliverd();
-// }
